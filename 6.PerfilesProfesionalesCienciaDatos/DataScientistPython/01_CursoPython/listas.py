a01735a1e99cdb91fe32d369b3c47f231f3098c8
Ariel <arielmakerlab@outlook.com>
2024-08-29 21:21:51 -0400

    Se continua el curso básico de python

---
+++ 6.PerfilesProfesionalesCienciaDatos/DataScientistPython/01_CursoPython/listas.py
@@ -0,0 +1,28 @@
+to_do = ["Dirigirnos al hotel",
+         "Ir a almorzar",
+         "Visitar un museo",
+         "Volver al hotel"]
+print(to_do)
+
+numbers = [1, 2, 3, 4, "cinco"]
+print(type(numbers))
+
+print(numbers)
+print(len(numbers))
+
+print("Quinto elemento:", numbers[4])
+print("Ultimo elemento:", numbers[-1])
+print("Rango de elementos:", numbers[1:3])
+print("Rango de elementos desde la posición 0:",
+      numbers[:4])
+print(numbers)
+numbers.append(False)
+print(numbers)
+
+numbers.insert(3, 3.5)
+print(numbers)
+
+print(numbers.index(False))
+
+del numbers[5:]
+print(numbers)
