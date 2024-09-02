29911a20677b509410cc85e8f70acbf6d5f928af
Ariel <arielmakerlab@outlook.com>
2024-09-02 05:42:16 -0400

    Se continua python en control de flujo

--- 6.PerfilesProfesionalesCienciaDatos/DataScientistPython/01_CursoPython/ControlFlujo.py
+++ 6.PerfilesProfesionalesCienciaDatos/DataScientistPython/01_CursoPython/ControlFlujo.py
@@ -32,3 +32,29 @@ if is_member:
 else:
     print('No eres miembro y NO TIENES ACCESO')
     
+
+# Bucles y control de iteraciones
+
+numbers = [1, 2, 3, 4, 5, 6]
+for i in numbers:
+    print("Aquí i es igual a:", i+1)
+    
+for i in range(3, 10):
+    print(i)
+    
+fruits =['Manzana', 'Pera', 'Uva', 'Naranja','Tomate']
+for fruit in fruits:
+    print(fruit)
+    if fruit == 'Naranja':
+        print('Naranja encontrada')
+        
+x3 = 0
+
+while x3<5:
+    if x3 == 3:
+        break
+    
+    print(x3)
+    x3 += 1
+    
+
